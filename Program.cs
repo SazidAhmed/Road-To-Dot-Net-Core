2ae52f4c6c6a2e2ef1ce9bfced3163053db8b302
SazidAhmed <sazidahmed.official@gmail.com>
2022-02-16 17:03:13 +0600

    user input done

--- Program.cs
+++ Program.cs
@@ -6,13 +6,30 @@ namespace TicketBooking
   {
     static void Main(string[] args)
     {
-      string conferenceName = "DotNet Core Conference";
-      int totalTickets = 10;
+      const string conferenceName = "DotNet Core Conference";
+      const int totalTickets = 10;
       int availableTickets = 10;
 
-      Console.WriteLine("Welcome to " + conferenceName + "booking application");
+      Console.WriteLine("Welcome to " + conferenceName + " booking application");
       Console.WriteLine("We have total of " + totalTickets + " tickets and " + availableTickets + " tickets still available");
-      Console.WriteLine("Get your ticket here to attend");
+      Console.WriteLine("Follow the steps to book your tickets");
+
+      Console.WriteLine("Enter your name : ");
+      string userName = Console.ReadLine();
+      
+      Console.WriteLine("Enter your email : ");
+      string userEmail = Console.ReadLine();
+
+      Console.WriteLine("How many tickets do you want to book ? : ");
+      string userTickets = Console.ReadLine();
+
+      availableTickets = availableTickets - Convert.ToInt32(userTickets);
+
+      Console.WriteLine("Thank you "+ userName + " for booking "+ userTickets + " tickets. You will recieve a confirmation email at " + userEmail);
+
+      Console.WriteLine(availableTickets + " Tickets available");
+
+
     }
   }
 }
