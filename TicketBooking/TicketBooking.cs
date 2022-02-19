f5fea9ec39b2751afe677d5d9cfffac31ef2891b
SazidAhmed <sazidahmed.official@gmail.com>
2022-02-19 20:00:48 +0600

    working on new booking logics

--- TicketBooking/TicketBooking.cs
+++ TicketBooking/TicketBooking.cs
@@ -20,25 +20,36 @@ namespace TicketBooking
     }
 
     public static void selectedInputs(){
+
+        Console.WriteLine("Enter your choice : ");
+        Console.WriteLine("=======================");
         
         string selectedOption = Console.ReadLine();
 
-        switch (selectedOption){
-            case "1":
-                Console.WriteLine("Total Tickets : " + totalTickets);
-                break;
+        if(selectedOption == "1"){
+            Console.WriteLine("Total Tickets : " + totalTickets);
+            selectedInputs();
+        }else if(selectedOption == "2"){
+            Console.WriteLine("Available Tickets : " + availableTickets);
+            selectedInputs();
+        }else if(selectedOption == "3"){
+            bookTicket(availableTickets);
+        }else{
+            Console.WriteLine("See you soon.");
+        }
+    }
 
-            case "2":
-                Console.WriteLine("Available Tickets : " + availableTickets);
-                break;
-
-            case "3":
-                bookTicket(availableTickets);
-                break;
-
-            case "4":
-                Console.WriteLine("See you soon.");
-                break;
+    public static void bookingsTicket(){
+        Console.WriteLine("How many tickets do you want to book ? : ");
+        string userTickets = Console.ReadLine();
+        int userTicket = Convert.ToInt32(userTickets);
+        if(userTicket > availableTickets){
+            Console.WriteLine("Only " + availableTickets + " tickets available");
+        }else{
+            var info = UserInfo.usInfo();
+            bookings.Add(info.Item1);
+            availableTickets = availableTickets - userTicket;
+            Console.WriteLine("Thank you "+ info.Item1 + " for booking "+ userTickets + " tickets. You will recieve a confirmation email at " + info.Item2);
         }
     }
 
@@ -49,26 +60,11 @@ namespace TicketBooking
       if(availableTickets <= 0){
         Console.WriteLine("All tickets are sold out!");
       }else{
-        int i = 0;
-        do{
-            Console.WriteLine("How many tickets do you want to book ? : ");
-            string userTickets = Console.ReadLine();
-            int userTicket = Convert.ToInt32(userTickets);
-            
-            if(userTicket > availableTickets){
-                Console.WriteLine("Only " + availableTickets + " tickets available");
-                continue;
-            }else{
-                var info = UserInfo.usInfo();
-                bookings.Add(info.Item1);
-                availableTickets = availableTickets - userTicket;
-                Console.WriteLine("Thank you "+ info.Item1 + " for booking "+ userTickets + " tickets. You will recieve a confirmation email at " + info.Item2);
-            }
+        bookingsTicket();
 
-            if(bookings.Count > 0){
-                displayBookings(bookings, availableTickets);
-            }
-        }while(i < availableTickets);
+        if(bookings.Count > 0){
+            displayBookings(bookings, availableTickets);
+        }
       }
     }
   }
